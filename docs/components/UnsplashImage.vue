69ef01f86811fb5f24d365469e67f17162994b32
nigelwhite <webdev@permaculture.org.uk>
2023-12-29 09:03:58 +0000

    unsplash nearly there

--- docs/components/UnsplashImage.vue
+++ docs/components/UnsplashImage.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <figure class="unsplash-image">
     <img :src="image" :alt="alt" />
-    <div class="credits">
+    <figcaption class="credits" :style="{ color: 'colour' }">
       Photo by
       <a class="photographer" :href="photographerLink">{{
         photographerName
       }}</a>
       on
       <a class="unsplash" :href="unsplashLink">Unsplash</a>
-    </div>
-  </div>
+    </figcaption>
+  </figure>
 </template>
 
 <script setup>
@@ -19,7 +19,19 @@ defineProps({
   photographerLink: String,
   photographerName: String,
   unsplashLink: String,
+  colour: String,
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.unsplash-image {
+  position: relative;
+}
+
+figcaption {
+  position: absolute;
+  bottom: 8px;
+  right: 16px;
+  font-size: 0.75em;
+}
+</style>
